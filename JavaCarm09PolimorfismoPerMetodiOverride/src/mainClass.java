fb51437771bc1c9b54a01551d04d83ffe4d24e5d
v-matoscano <v-matoscano@ROMR90F84D0.SEA.CORP.EXPECN.com>
2015-07-22 17:19:16 +0200

    Initial commit

---
+++ JavaCarm09PolimorfismoPerMetodiOverride/src/mainClass.java
@@ -0,0 +1,14 @@
+
+public class mainClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Dipendente d = new Dipendente("Mario","Toscano");
+		System.out.println(d.toString());
+		
+		
+
+	}
+
+}
